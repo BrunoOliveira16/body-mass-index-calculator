9646ab019e311c30b01fcbb410ad5244adf13e82
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-10-02 17:39:26 -0300

    :recycle: Refactor: adicionado props de checked e onChange para o componente

--- src/components/InputRadio/index.tsx
+++ src/components/InputRadio/index.tsx
@@ -4,12 +4,27 @@ type PropsInputRadio = {
   label: string
   id: string
   name: string
+  checked: boolean
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const InputRadio = ({ label, id, name }: PropsInputRadio) => {
+const InputRadio = ({
+  label,
+  id,
+  name,
+  checked,
+  onChange
+}: PropsInputRadio) => {
   return (
     <S.Wrapper>
-      <S.Input type="radio" id={id} title={id} name={name} />
+      <S.Input
+        type="radio"
+        id={id}
+        title={id}
+        name={name}
+        checked={checked}
+        onChange={onChange}
+      />
       <S.Label htmlFor={id}>{label}</S.Label>
     </S.Wrapper>
   )
