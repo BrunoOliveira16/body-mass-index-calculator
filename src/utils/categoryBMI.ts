28a91246b1fae8d6c67004e49c5433ba58a75309
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-10-04 15:12:00 -0300

    :sparkles: Feat: criado objeto para categoria e faixas de IMC

---
+++ src/utils/categoryBMI.ts
@@ -0,0 +1,18 @@
+export const categoryMetricBMI = [
+  {
+    limit: 18.5,
+    category: 'está abaixo do peso'
+  },
+  {
+    limit: 24.9,
+    category: 'tem um peso saudável'
+  },
+  {
+    limit: 29.9,
+    category: 'está com excesso de peso'
+  },
+  {
+    limit: Infinity,
+    category: 'está Obeso'
+  }
+]
