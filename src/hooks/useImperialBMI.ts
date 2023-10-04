8c858af9e03ddbcf15ce493c7dd696848d1e973d
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-10-04 15:13:54 -0300

    :sparkles: Feat: criado metodo para calcular faixa de peso min-max ideal de acordo com a altura do usuario

--- src/hooks/useImperialBMI.ts
+++ src/hooks/useImperialBMI.ts
@@ -1,14 +1,22 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useBMIContext, BMIContextType } from '../context/BMIContext'
 
 const useImperialBMI = () => {
   const { heightImperial, weightImperial, setInputIsEmpty, setResultBMI } =
     useBMIContext() as BMIContextType
+  const [idealWeightRange, setIdealWeightRange] = useState<{
+    minWeight: { st: number; lbs: number }
+    maxWeight: { st: number; lbs: number }
+  }>({
+    minWeight: { st: 0, lbs: 0 },
+    maxWeight: { st: 0, lbs: 0 }
+  })
 
   const isAllZero =
     (heightImperial.ft === 0 && heightImperial.in === 0) ||
     (weightImperial.st === 0 && weightImperial.lbs === 0)
 
+  // Função para calcular o IMC
   const calculateImperialBMI = (
     height: { ft: number; in: number },
     weight: { st: number; lbs: number }
@@ -22,15 +30,37 @@ const useImperialBMI = () => {
     return result
   }
 
+  // Função para calcular o peso minimo e maximo para um IMC saudável
+  const calculateIdealWeightRange = (height: { ft: number; in: number }) => {
+    const totalHeightInMeters = (height.ft * 12 + height.in) * 0.0254
+    const minWeightKg = 18.5 * totalHeightInMeters * totalHeightInMeters
+    const maxWeightKg = 24.5 * totalHeightInMeters * totalHeightInMeters
+
+    //converter o peso de kg para st e lbs
+    const minWeightSt = Math.floor(minWeightKg / 6.35029)
+    const minWeightLbs = Math.round((minWeightKg % 6.35029) / 0.453592)
+    const maxWeightSt = Math.floor(maxWeightKg / 6.35029)
+    const maxWeightLbs = Math.round((maxWeightKg % 6.35029) / 0.453592)
+
+    return {
+      minWeight: { st: minWeightSt, lbs: minWeightLbs },
+      maxWeight: { st: maxWeightSt, lbs: maxWeightLbs }
+    }
+  }
+
   useEffect(() => {
     const isEmpty = isAllZero
     setInputIsEmpty(isEmpty)
 
     if (!isEmpty) {
       const result = calculateImperialBMI(heightImperial, weightImperial)
+      const rangeResult = calculateIdealWeightRange(heightImperial)
       setResultBMI(result)
+      setIdealWeightRange(rangeResult)
     }
   }, [setInputIsEmpty, setResultBMI, isAllZero, heightImperial, weightImperial])
+
+  return { ...useBMIContext(), idealWeightRange }
 }
 
 export default useImperialBMI
--- src/hooks/useMetricBMI.ts
+++ src/hooks/useMetricBMI.ts
@@ -1,17 +1,31 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useBMIContext, BMIContextType } from '../context/BMIContext'
 
 const useMetricBMI = () => {
   const { heightMetric, weightMetric, setResultBMI, setInputIsEmpty } =
     useBMIContext() as BMIContextType
+  const [idealWeightRange, setIdealWeightRange] = useState<{
+    minWeight: number
+    maxWeight: number
+  }>({ minWeight: 0, maxWeight: 0 })
 
   useEffect(() => {
     const isAllZero = heightMetric === 0 || weightMetric === 0
 
+    // Função para calcular o IMC
     const calculateBMI = (height: number, weight: number): number => {
       const heightInMeters = height / 100
       const result = weight / (heightInMeters * heightInMeters)
       return result
+    }
+
+    // Função para calcular o peso minimo e maximo para um IMC saudável
+    const calculateIdealWeightRange = (height: number) => {
+      const heightInMeters = height / 100
+      const minWeight = 18.5 * heightInMeters * heightInMeters
+      const maxWeight = 24.9 * heightInMeters * heightInMeters
+
+      return { minWeight, maxWeight }
     }
 
     const isEmpty = isAllZero
@@ -19,9 +33,13 @@ const useMetricBMI = () => {
 
     if (!isEmpty) {
       const result = calculateBMI(heightMetric, weightMetric)
+      const rangeResult = calculateIdealWeightRange(heightMetric)
       setResultBMI(result)
+      setIdealWeightRange(rangeResult)
     }
   }, [heightMetric, setInputIsEmpty, setResultBMI, weightMetric])
+
+  return { ...useBMIContext(), idealWeightRange }
 }
 
 export default useMetricBMI
